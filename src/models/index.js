b737729d6b93e412453d34a8692fbd536398bd74
Minh Hieu <buihieu@quangich.com>
2021-05-28 16:46:12 +0700

    init

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+import Comment from './Comment';
+import Follow from './Follow';
+import Like from './Like';
+import Message from './Message';
+import Notification from './Notification';
+import Post from './Post';
+import User from './User';
+
+export default {
+  User,
+  Post,
+  Like,
+  Follow,
+  Comment,
+  Notification,
+  Message,
+};
